91bdcdd73fc8adb2b7eae5f12411b3adc176b164
Ali Valishov <108894573+dorpen6@users.noreply.github.com>
2024-07-10 03:29:59 +0400

    Level 2 Player Movement UPDATE

--- Player.gd
+++ Player.gd
@@ -5,6 +5,18 @@ var speed
 @export var run_speed = 10.0
 @export var jump = 5.0
 @export var gravity = 9.8
+@export var sensitivity = 0.003
+@onready var head = $Head
+@onready var camera = $Head/Camera
+
+func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+func _unhandled_input(event):
+	if event is InputEventMouseMotion:
+		head.rotate_y(-event.relative.x * sensitivity)
+		camera.rotate_x(-event.relative.y * sensitivity)
+		camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-40), deg_to_rad(60))
 
 func _physics_process(delta):
 	# GRAVITY
@@ -25,7 +37,7 @@ func _physics_process(delta):
 	var input_dir = Input.get_vector("left", "right", "forward", "backward")
 	
 	# APPLY IT TO 3D
-	var directions = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	var directions = (head.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	
 	if directions:
 		velocity.x = directions.x * speed
