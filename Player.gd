0eba6cf58db48aaf1a495406bc733b7ba876b269
Ali Valishov <108894573+dorpen6@users.noreply.github.com>
2024-07-10 15:32:20 +0400

    Level 3 Player Movement UPDATE

--- Player.gd
+++ Player.gd
@@ -3,7 +3,7 @@ extends CharacterBody3D
 var speed
 @export var walk_speed = 5.0
 @export var run_speed = 10.0
-@export var jump = 5.0
+@export var jump = 3.0
 @export var gravity = 9.8
 @export var sensitivity = 0.003
 @export var bob_freq = 2.0
@@ -11,6 +11,9 @@ var speed
 @export var t_bob = 0.0
 @onready var head = $Head
 @onready var camera = $Head/Camera
+@export var base_fov = 75.0
+@export var fov_change = 1.5
+
 
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
@@ -35,11 +38,9 @@ func _physics_process(delta):
 		speed = run_speed
 	else:
 		speed = walk_speed
-		
+	
 	# GET INPUT DIRECTIONS 2D
 	var input_dir = Input.get_vector("left", "right", "forward", "backward")
-	
-	# TRANSFORM IT TO 3D
 	var direction = (head.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	
 	if is_on_floor():
@@ -52,14 +53,16 @@ func _physics_process(delta):
 	else:
 		velocity.x = lerp(velocity.x, direction.x * speed, delta * 3.0)
 		velocity.z = lerp(velocity.z, direction.z * speed, delta * 3.0)
-		
-		
+	
 	# HEAD BOB
 	t_bob += delta * velocity.length() * float(is_on_floor())
 	camera.transform.origin = headbob(t_bob)
 	
+	# FOV
+	var velocity_clamped = clamp(velocity.length(), 0.5, run_speed * 2 )
+	var target_fov = base_fov + fov_change * velocity_clamped
+	
 	move_and_slide()
-
 func headbob(time) -> Vector3:
 	var pos = Vector3.ZERO
 	pos.y = sin(time * bob_freq) * bob_amp
